2dc86e55c45256c1d314de75a9fd3c2455ff86bb
dieptonn <diep.bt194505@sis.hust.edu.vn>
2023-06-21 04:45:17 +0700

    xong phần tính toán theo %, đưa ra tất cả bản ghi theo thứ tự giảm dần, respone time hơi lâu (15s)

--- src/services/matching.js
+++ src/services/matching.js
@@ -10,153 +10,143 @@ const searchStaff = async (filters) => {
 
             const totalRating = staff.rating.reduce(
                 (sum, ratingObj) => sum + ratingObj.star,
-                0,
+                0
             );
             const averageRating = totalRating / staff.rating.length;
             return averageRating;
         }
 
-        function compareExperience(employeeExperience, filterExperience) {
-            const experienceValues = {
-              'non': 0,
-              '1 years': 1,
-              '2 years': 2,
-              '3 years': 3,
-              '> 3 years': Infinity
-            };
-          
-            const employeeYears = experienceValues[employeeExperience];
-            const filterYears = experienceValues[filterExperience];
-          
-            return employeeYears >= filterYears;
-          }
-
         const employees = await Staff.find({});
 
         async function calculateMatchingScore(employee, filters) {
             let matchingScore = 0;
-            let numFailedConditions = 0;
+            let matchingScore1 = 0;
+            let matchingScore2 = 0;
+            let matchingScore3 = 0;
+            let matchingScore4 = 0;
+            let matchingScore5 = 0;
 
-            const { rating, salary, userLanguage, careExp, cookExp, address } =
-                filters;
+            const { rating, salary, userLanguage, careExp, cookExp, address } = filters;
             const averageRating = calculateAverageRating(employee);
 
             if (rating !== undefined) {
-                const hasMatchingRating = averageRating >= rating;
-                if (hasMatchingRating) {
-                    matchingScore += 1.75;
+                if (averageRating === rating) {
+                    matchingScore1 = 100;
+                } else if (averageRating < rating) {
+                    matchingScore1 = (averageRating * 100) / rating;
                 } else {
-                    numFailedConditions += 1;
+                    matchingScore1 =
+                        ((rating - (averageRating - rating)) * 100) / rating;
                 }
             }
 
             if (userLanguage !== undefined) {
-                const userLanguageName = await Language.find({
-                    _id: { $in: employee.userLanguage },
-                }).distinct('name');
-                const isMatchingLanguage =
-                    userLanguageName.includes(userLanguage);
-                if (isMatchingLanguage) {
-                    matchingScore += 2;
-                } else {
-                    numFailedConditions += 1;
-                }
+                const userLanguages = Array.isArray(userLanguage) ? userLanguage : [userLanguage];
+                const userLanguageNames = await Language.find({ _id: { $in: employee.userLanguage } }).distinct('name');
+                const matchingLanguageCount = userLanguages.reduce((count, lang) => {
+                    if (userLanguageNames.includes(lang)) {
+                        return count + 1;
+                    }
+                    return count;
+                }, 0);
+                matchingScore2 = (matchingLanguageCount / userLanguages.length) * 100;
+                // console.log(matchingScore2);
             }
 
             if (salary !== undefined) {
-                if (employee.salary <= salary) {
-                    matchingScore++;
+                if (employee.salary === salary) {
+                    matchingScore3 = 100;
+                } else if (employee.salary < salary) {
+                    matchingScore3 = (employee.salary * 100) / salary;
                 } else {
-                    numFailedConditions++;
+                    matchingScore3 =
+                        ((salary - (employee.salary - salary)) * 100) / salary;
                 }
             }
 
             if (careExp !== undefined) {
-                if (compareExperience(employee.careExp, careExp)) {
-                    matchingScore += 1.25;
+                const experienceValues = {
+                    non: 0,
+                    '1 years': 1,
+                    '2 years': 2,
+                    '3 years': 3,
+                    '> 3 years': 4,
+                };
+                const employeeYears = experienceValues[employee.careExp];
+                const filterYears = experienceValues[careExp];
+                if (employeeYears === filterYears) {
+                    matchingScore4 = 100;
+                } else if (employeeYears > filterYears) {
+                    matchingScore4 =
+                        ((filterYears - (employeeYears - filterYears)) * 100) /
+                        filterYears;
                 } else {
-                    numFailedConditions += 1;
+                    matchingScore4 = (employeeYears * 100) / filterYears;
                 }
             }
 
             if (cookExp !== undefined) {
-                if (compareExperience(employee.cookExp, cookExp)) {
-                    matchingScore += 1.25;
+                const experienceValues = {
+                    non: 0,
+                    '1 years': 1,
+                    '2 years': 2,
+                    '3 years': 3,
+                    '> 3 years': 4,
+                };
+                const employeeYears = experienceValues[employee.cookExp];
+                const filterYears = experienceValues[cookExp];
+                if (employeeYears === filterYears) {
+                    matchingScore5 = 100;
+                } else if (employeeYears > filterYears) {
+                    matchingScore5 =
+                        ((filterYears - (employeeYears - filterYears)) * 100) /
+                        filterYears;
                 } else {
-                    numFailedConditions += 1;
+                    matchingScore5 = (employeeYears * 100) / filterYears;
                 }
             }
 
-            return { matchingScore, numFailedConditions };
+            matchingScore =
+                (matchingScore1 +
+                    matchingScore2 +
+                    matchingScore3 +
+                    matchingScore4 +
+                    matchingScore5) /
+                5;
+
+            return matchingScore;
         }
 
         async function searchEmployees(filters) {
             const matchingPromises = employees.map((employee) =>
-                calculateMatchingScore(employee, filters),
+                calculateMatchingScore(employee, filters)
             );
             const matchingResults = await Promise.all(matchingPromises);
-
+        
             const matchedEmployees = [];
             for (let i = 0; i < employees.length; i++) {
                 const employee = employees[i];
-                const { matchingScore, numFailedConditions } =
-                    matchingResults[i];
-                if (numFailedConditions < 1) {
-                    const userLanguageName = await Language.find({
-                        _id: { $in: employee.userLanguage },
-                    }).distinct('name');
-                    const employeeWithuserLanguageName = {
-                        ...employee._doc,
-                        userLanguageName,
-                    };
-                    matchedEmployees.push(employeeWithuserLanguageName);
-                }
+                const matchingScore = matchingResults[i];
+                const userLanguageNames = await Language.find({
+                    _id: { $in: employee.userLanguage },
+                }).distinct('name');
+                const employeeWithuserLanguageName = {
+                    ...employee._doc,
+                    userLanguageNames,
+                    matchingScore: matchingScore.toFixed(2) + '%',
+                };
+                matchedEmployees.push(employeeWithuserLanguageName);
             }
+        
+            // Sắp xếp danh sách nhân viên theo điểm phù hợp giảm dần
+            // matchedEmployees.sort((a, b) => b.matchingScore - a.matchingScore);
+            matchedEmployees.sort((a, b) => parseFloat(b.matchingScore) - parseFloat(a.matchingScore));
 
-            return matchedEmployees.sort((a, b) => {
-                const scoreA = calculateMatchingScore(a, filters);
-                const scoreB = calculateMatchingScore(b, filters);
-                return scoreB - scoreA;
-            });
-        }
-
-        async function searchFailEmployees(filters) {
-            const matchingPromises = employees.map((employee) =>
-                calculateMatchingScore(employee, filters),
-            );
-            const matchingResults = await Promise.all(matchingPromises);
-
-            const matchedEmployees = [];
-            for (let i = 0; i < employees.length; i++) {
-                const employee = employees[i];
-                const { matchingScore, numFailedConditions } =
-                    matchingResults[i];
-                if (numFailedConditions > 0) {
-                    const userLanguageName = await Language.find({
-                        _id: { $in: employee.userLanguage },
-                    }).distinct('name');
-                    const employeeWithuserLanguageName = {
-                        ...employee._doc,
-                        userLanguageName,
-                    };
-                    matchedEmployees.push(employeeWithuserLanguageName);
-                }
-            }
-
-            matchedEmployees.sort((a, b) => {
-                const scoreA = calculateMatchingScore(a, filters);
-                const scoreB = calculateMatchingScore(b, filters);
-                return scoreB - scoreA;
-            });
-            const output = matchedEmployees.slice(0, 3);
-            return output;
+        
+            return matchedEmployees;
         }
 
         let matchedStaffs = await searchEmployees(filters);
-
-        if (matchedStaffs.length === 0) {
-            matchedStaffs = await searchFailEmployees(filters);
-        }
 
         return matchedStaffs;
     } catch (error) {
